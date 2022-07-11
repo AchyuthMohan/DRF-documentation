ada9dd78a1af5f2580926c39d6d6079209899b9d
Achyuth Mohan <achyuthmohan1@gmail.com>
2022-07-11 23:40:46 +0530

    added env and 1:13:12 completed

--- api/views.py
+++ api/views.py
@@ -9,7 +9,21 @@ from .serializers import ArticleSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 # Create your views here.
+
+class ArticleAPIView(APIView):
+    def get(self,request):
+        articles=Article.objects.all()
+        serializer=ArticleSerializer(articles,many=True)
+        return Response(serializer.data)
+
+    def post(self,request):
+        serializer=ArticleSerializer(data=request.data)
+
+
+
+
 @api_view(['GET','POST'])
 def article_list(request):
     if request.method=='GET':
