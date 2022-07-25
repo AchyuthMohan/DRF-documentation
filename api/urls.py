f887f9d9cac73cc150d1253cf9f019618e705d7d
Achyuth Mohan <achyuthmohan1@gmail.com>
2022-07-25 23:25:22 +0530

    class based views

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import article_list,article_detail
+from .views import article_list,article_detail,ArticleListAPI ,ArticleDetails
 urlpatterns = [
-    path('article/',article_list),
-    path('detail/<int:pk>/',article_detail),
+    # path('article/',article_list),
+    path('article/',ArticleListAPI.as_view()),
+    path('detail/<int:id>/',ArticleDetails.as_view()),
+    # path('detail/<int:pk>/',article_detail),
+    
 
 ]
--- api/views.py
+++ api/views.py
@@ -12,7 +12,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 # Create your views here.
 
-class ArticleAPIView(APIView):
+class ArticleListAPI(APIView):
     def get(self,request):
         articles=Article.objects.all()
         serializer=ArticleSerializer(articles,many=True)
@@ -21,9 +21,40 @@ class ArticleAPIView(APIView):
     def post(self,request):
         serializer=ArticleSerializer(data=request.data)
 
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+
+class ArticleDetails(APIView):
+    def get_object(self, id):
+        try:
+            return Article.objects.get(id=id)
+        except Article.DoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self,request,id):
+        article=self.get_object(id)
+        serializer=ArticleSerializer(article)
+        return Response(serializer.data)
+
+    def put(self, request,id):
+        article=self.get_object(id)
+        serializer=ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self,request,id):
+        article=self.get_object(id)
+        article.delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
 
 
 
+           # Funtion Based views...
 @api_view(['GET','POST'])
 def article_list(request):
     if request.method=='GET':
